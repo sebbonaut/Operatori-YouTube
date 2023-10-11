7423bb63747ac6d32ace0d42c4110f72a1c67dd7
sebas <sebas@DESKTOP-S92RB8H>
2023-10-11 15:09:56 +0200

    Add project files.

---
+++ Operatori/Program.cs
@@ -0,0 +1,34 @@
+﻿using Operatori;
+
+Razlomak[] razlomci = new Razlomak[3];
+razlomci[0] = new Razlomak(-4, 6);
+razlomci[1] = new Razlomak(-1, 3);
+//razlomci[2] = razlomci[0] + razlomci[1];
+//razlomci[2] = razlomci[0] / razlomci[0];
+razlomci[1] += razlomci[0];
+razlomci[1] += (Razlomak)2;
+razlomci[2] = new Razlomak(-2, 3);
+Console.WriteLine(razlomci[2].Brojnik + "/"
+    + razlomci[2].Nazivnik);
+//Console.WriteLine(2 + razlomci[2]);
+razlomci[0] = new Razlomak(5,3);
+if (razlomci[0])
+    Console.WriteLine("Istina");
+else if (!razlomci[0])
+    Console.WriteLine("Laz");
+else
+    Console.WriteLine("Nesto drugo");
+Console.WriteLine(razlomci[0].ToString());
+
+razlomci[0] = new Razlomak(-2, 6);
+//razlomci[1] = razlomci[0];
+razlomci[1] = new Razlomak(-2, 6);
+if (razlomci[0].Equals(razlomci[1]))
+    Console.WriteLine("Isti su!");
+else
+    Console.WriteLine("Nisu isti!");
+
+int a = 2, b = 3;
+if ((a++) == 2 || (b++) == 3) //short-circuit
+    Console.WriteLine("ok");
+Console.WriteLine($"{a}, {b}");
---
+++ Operatori/Razlomak.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatori
+{
+    internal class Razlomak
+    {
+        int brojnik, nazivnik;
+        public static Razlomak operator +(Razlomak a, Razlomak b)
+            => new(a.Brojnik * b.Nazivnik + a.Nazivnik * b.Brojnik,
+                a.Nazivnik * b.Nazivnik);
+        public static Razlomak operator -(Razlomak a, Razlomak b)
+            => new(a.Brojnik * b.Nazivnik - a.Nazivnik * b.Brojnik,
+                a.Nazivnik * b.Nazivnik);
+        public static Razlomak operator *(Razlomak a, Razlomak b)
+            => new(a.Brojnik * b.Brojnik,
+                a.Nazivnik * b.Nazivnik);
+        public static Razlomak operator /(Razlomak a, Razlomak b)
+        {
+            if (b.Brojnik == 0)
+                throw new DivideByZeroException("Dijeljenje nulom!");
+            return (b.Brojnik < 0) ?
+                new(-a.Brojnik * b.Nazivnik, a.Nazivnik * -b.Brojnik) :
+                new(a.Brojnik * b.Nazivnik, a.Nazivnik * b.Brojnik);
+        }
+        public static explicit operator Razlomak(int n)
+            => new Razlomak(n, 1);
+        /* public static implicit operator double(Razlomak a)
+            => a.Brojnik / (1.0 * a.Nazivnik); */
+        public static bool operator true(Razlomak a)
+            => a.Brojnik == 1 && a.Nazivnik == 1;
+        public static bool operator false(Razlomak a)
+            => a.Brojnik == 0;
+        public static Razlomak operator !(Razlomak a)
+        {
+            if (a.Brojnik == 0)
+                return new(1, 1);
+            else if (a.Brojnik == 1 && a.Nazivnik == 1)
+                return new(0, 1);
+            else
+                return new(2, 1);
+        }
+        public override string ToString()
+            => Brojnik + " / " + Nazivnik;
+        void Skrati()
+        {
+            for (int i = 2; i <= nazivnik; i++)
+            {
+                while (brojnik % i == 0 && nazivnik % i == 0)
+                {
+                    brojnik /= i;
+                    nazivnik /= i;
+                }
+            }
+        }
+        public int Brojnik
+        {
+            get => brojnik;
+            set
+            {
+                brojnik = value;
+                Skrati();
+            }
+        }
+        public int Nazivnik
+        {
+            get => nazivnik;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Nazivnik nije pozitivan!");
+                }
+                nazivnik = value;
+                Skrati();
+            }
+        }
+        public Razlomak(int brojnik, int nazivnik)
+        {
+            Brojnik = brojnik;
+            Nazivnik = nazivnik;
+        }
+
+        /* public static bool operator ==(Razlomak a, Razlomak b)
+            => a.Brojnik == b.Brojnik && a.Nazivnik == b.Nazivnik;
+        public static bool operator !=(Razlomak a, Razlomak b)
+            => !(a == b); */
+        public override bool Equals(object? obj)
+        {
+            return obj is Razlomak && Equals(obj);
+        }
+        public bool Equals(Razlomak a)
+            => Brojnik == a.Brojnik && Nazivnik == a.Nazivnik;
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Brojnik, Nazivnik);
+        }
+    }
+}
